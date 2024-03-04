afc489adae13740e7980ec18f24f6812dd081f2d
nikhatparveen001 <nikhatparveen88774@gmail.com>
2024-03-04 12:58:01 +0530

    Modified After Cloning

--- Launch.java
+++ Launch.java
@@ -4,4 +4,10 @@ class Launch
     {
        System.out.println("Hello World");
      }
+      public static void add(){
+             int a=10;
+             int b=20;
+             int sum=a+b;
+             System.out.println("Sum is: "+sum);
+         }
 }
